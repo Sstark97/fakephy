b8b0df7abb2a7600f005fbde8be9663760500d98
Sstark97 <aitorscinfo@gmail.com>
2022-09-14 20:02:29 +0100

    Fix SearchBar heigth when search a gif in mobile

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as Logo } from "@assets/fakephy.svg";
 
 const Banner = () => {
   return (
-    <div className="flex justify-center items-center h-1/2 py-3">
+    <div className="flex justify-center items-center h-2/6 py-3">
       <Logo className= "w-11 h-11"/>
       <span className="text-white">Fakephy</span>
     </div>
--- src/components/ContentList.tsx
+++ src/components/ContentList.tsx
@@ -6,7 +6,7 @@ import Pagination from "@containers/Pagination";
 const ContentList: FC<ContentListProps> = ({ content }) => {
   return (
     <>
-    <ul className="flex flex-wrap gap-x-3 gap-y-3 h-[90%] overflow-y-auto justify-center py-10 xl:scrollbar scrollbar-thumb-gray-900 scrollbar-track-transparent">
+    <ul className="flex flex-wrap gap-x-3 gap-y-3 h-[90%] overflow-y-auto justify-center py-4 lg:py-8 xl:scrollbar scrollbar-thumb-gray-900 scrollbar-track-transparent">
       {content.map((item: Gif) => (
         <Content
           key={item.id}
